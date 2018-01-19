9e0cf4d6d660f1958c9149715bb2af5a2462871c
Kevin Lee <timpaniman@gmail.com>
2018-01-19 12:46:15 -0800

    2nd

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => ( <
+    header className = "header" >
+    <
+    h2 > Memory Game! < /h2> <
+    h3 >
+    Click on an image to increase points, but don 't click on any more than once! <
+    /h3> <
+    /header>
+);
+
+export default Header;
